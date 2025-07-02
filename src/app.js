c7c80c29d53a2c8aa1ee69f8f42df1311a692dcc
Yash Kshitiz <yashkshitiz21@gmail.com>
2025-07-02 22:30:13 +0530

    Added the template for remaining controllers and routes

--- src/app.js
+++ src/app.js
@@ -21,7 +21,24 @@ app.use(cookieParser());
 
 
 import userRouter from "./routes/user_route.js";
+import healthcheckRouter from "./routes/healthcheck_route.js"
+import tweetRouter from "./routes/tweet_route.js"
+import subscriptionRouter from "./routes/subscription_route.js"
+import videoRouter from "./routes/video_route.js"
+import commentRouter from "./routes/comment_route.js"
+import likeRouter from "./routes/like_route.js"
+import playlistRouter from "./routes/playlist_route.js"
+import dashboardRouter from "./routes/dashboard_route.js"
 
 app.use("/api/v1/users", userRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 export { app };
---
+++ src/models/like_model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const likeSchema = new mongoose.Schema(
+  {
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    comment: {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+    tweet: {
+      type: Schema.Types.ObjectId,
+      ref: "Tweet",
+    },
+    likedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+// likeSchema.plugin(mongooseAggregatePaginate);
+export const Like = mongoose.model("Like", likeSchema);
